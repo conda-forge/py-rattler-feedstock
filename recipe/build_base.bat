b8662cb0a6ebacc866e9a4047040a43233953ef1
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-11-05 11:40:50 +0100

    fix: use native-tls on conda-forge instead of the default rust-tls

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,6 +1,8 @@
 @echo on
 set "PYO3_PYTHON=%PYTHON%"
 
+set "MATURIN_SETUP_ARGS=--features native-tls"
+
 set "CMAKE_GENERATOR=NMake Makefiles"
 maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON%
 if errorlevel 1 exit 1
