af009affea6635f91f8fe52846d9ef98b1613e1d
Pavel Zwerschke <pavelzw@gmail.com>
2024-12-16 10:59:40 +0100

    fix

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -7,7 +7,9 @@ set CARGO_PROFILE_RELEASE_LTO=fat
 set "CMAKE_GENERATOR=NMake Makefiles"
 maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
 
-%PYTHON% -m pip install --no-deps --ignore-installed target\wheels\*.whl -vv || exit 1
-
 cd py-rattler
+
+FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set py_rattler_wheel=%%i
+%PYTHON% -m pip install --ignore-installed --no-deps %py_rattler_wheel% -vv || exit 1
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || exit 1
