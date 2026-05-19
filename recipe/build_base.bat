1d22186fa57f0dfa78001677e859ed74c11caa3e
Pavel Zwerschke <pavelzw@gmail.com>
2026-05-19 10:37:10 +0200

    use cargo-auditable

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -3,6 +3,10 @@ set "PYO3_PYTHON=%PYTHON%"
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
+@rem Remove this wrapper once https://github.com/conda-forge/rust-activation-feedstock/pull/79 is merged
+copy %RECIPE_DIR%\cargo-auditable-wrapper.bat %BUILD_PREFIX%\Library\bin\cargo-auditable-wrapper.bat
+if %ERRORLEVEL% neq 0 exit 1
+set "CARGO=cargo-auditable-wrapper.bat"
 
 set "CMAKE_GENERATOR=NMake Makefiles"
 maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
